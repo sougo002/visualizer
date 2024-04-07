762e6f2b59fac6db947d53f03f0388652740e0bb
sougo <sougo_002@outlook.jp>
2024-04-07 21:36:49 +0900

    change: base path

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,27 @@ import { vitePlugin as remix } from "@remix-run/dev";
 import { installGlobals } from "@remix-run/node";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { copyFileSync } from "node:fs";
+import { join } from "node:path";
 
 installGlobals();
 
 export default defineConfig({
-  plugins: [remix({ ssr: false }), tsconfigPaths()],
+  base: "/vite-base/",
+  plugins: [
+    remix({
+      ssr: false,
+      basename: "/vite-base/remix-base",
+      buildEnd(args) {
+        if (!args.viteConfig.isProduction) return;
+        // NOTE: https://github.com/brookslybrand/remix-gh-pages/blob/ce5981bbdf29fa0c4adbdb1634f8ae05c6af9609/vite.config.ts#L16-L23
+        const buildPath = args.viteConfig.build.outDir;
+        copyFileSync(
+          join(buildPath, "index.html"),
+          join(buildPath, "404.html")
+        );
+      },
+    }),
+    tsconfigPaths(),
+  ],
 });
